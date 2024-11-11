63cfea987c990700bdc0b3dacafb5da70b345ac5
Mctaowing <mikkelchristensen2001@gmail.com>
2024-11-11 12:21:30 +0100

    Added camera and set game as main scene

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -40,6 +40,3 @@ func updateAnimation():
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
-	
-func _physics_process(delta: float) -> void:
-	pass
