51247fbd1226193153564f99693bc4f587a6698a
Mctaowing <mikkelchristensen2001@gmail.com>
2024-11-11 14:24:57 +0100

    Enemy created
    Added assets for enemies, created enemy and added an idle animation

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,13 +1,16 @@
 extends CharacterBody2D
 
 @export var speed = 200
-var direction = ""
+var direction
+var health
+var damage
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	health = 200
+	damage = 20
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,7 +18,6 @@ func _process(delta: float) -> void:
 	move_and_slide()
 	updateDirection()
 	updateAnimation()
-
 
 func updateDirection():
 	if velocity.x < 0: 
@@ -28,7 +30,7 @@ func updateDirection():
 		direction = "backward"
 	elif velocity.y > 0:
 		direction = "forward"
-		
+
 func updateAnimation():
 	if velocity.length() == 0 && !animated_sprite_2d.is_playing():
 		animated_sprite_2d.play("Idle_" + direction)
@@ -36,7 +38,7 @@ func updateAnimation():
 		animated_sprite_2d.play("Walk_" + direction)
 	elif Input.is_action_just_pressed("attack") == true:
 		animated_sprite_2d.play("Attack_" + direction)
-	
+
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
