6fb9c06fa7a74a1bf347e4eb35e2cc8c811ac05e
Mctaowing <mikkelchristensen2001@gmail.com>
2024-11-19 14:04:32 +0100

    startet y-sorting and changing tiles

--- Scenes/Orc1/orc_1.gd
+++ Scenes/Orc1/orc_1.gd
@@ -50,3 +50,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
+
+func enemy():
+	pass
--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -8,6 +8,11 @@ var direction = "forward"
 var health
 var damage
 
+var enemy_in_attack_range = false
+var enemy_attack_cooldown = true
+var player_alive = true
+
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
@@ -22,6 +27,13 @@ func _process(_delta: float) -> void:
 	updateDirection()
 	updateAnimation()
 	coordinates = character_body_2d.position
+	enemy_attack()
+	
+	if health <= 0:
+		player_alive = false ##Can add end screen or game over screen etc.
+		health = 0
+		print("player has been killed")
+		animated_sprite_2d.play("Death")
 
 func updateDirection():
 	if velocity.x < 0: 
@@ -46,3 +58,27 @@ func updateAnimation():
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+
+
+func _on_player_hitbox_body_entered(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_in_attack_range = true
+
+
+func _on_player_hitbox_body_exited(body: Node2D) -> void:
+	if body.has_method("enemy"):
+		enemy_in_attack_range = false
+
+func enemy_attack():
+	if enemy_in_attack_range and enemy_attack_cooldown == true:
+		health = health - 10
+		enemy_attack_cooldown = false
+		$Attack_cooldown.start()
+		print(health)
+
+func player():
+	pass
+
+
+func _on_attack_cooldown_timeout() -> void:
+	enemy_attack_cooldown = true
