7202d474afb9b5d733f2722ad10868146c1079da
Mctaowing <mikkelchristensen2001@gmail.com>
2024-11-25 13:14:12 +0100

    interaction area and manager started

---
+++ Scenes/Interaction/interaction_manager.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var label = $Label
+
+const base_text = "[E] to "
+
+var active_areas = []
+var can_interact = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if active_areas > 0 && can_interact:
+		active_areas.sort_custom(sort_by_distance_to_player)
+		label.text = base_text + active_areas[0].action_name
+		label.global_position = active_areas[0].global_position
+		label.global_position.y -= 36
+		label.global_position.x -= label.size.x / 2
+		label.show()
+	else:
+		label.hide()
+
+
+func register_area(area: InteractionArea):
+	active_areas.push_back(area)
+
+
+func unregiser_arae(area: InteractionArea):
+	var index = active_areas.find(area)
+	if index != -1:
+		active_areas.remove_at(index)
+
+
+func sort_by_distance_to_player(area1, area2):
+	var area1_to_player = player.global_position.distance_to(area1.global_position)
+	var area2_to_player = player.global_position.distance_to(area2.global_position)
+	return area1_to_player < area2_to_player
