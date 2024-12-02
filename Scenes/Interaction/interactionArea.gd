4314c46f502c4dcc3774ccbeacefc2390a9ebd33
Mille <Mille_n.alstroem@hotmail.com>
2024-12-02 16:58:57 +0100

    idk noget ignore af warnings

--- Scenes/Interaction/interactionArea.gd
+++ Scenes/Interaction/interactionArea.gd
@@ -14,13 +14,16 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
 	pass
 
 
+@warning_ignore("unused_parameter")
 func _on_body_entered(body: Node2D) -> void:
 	InteractionManager.register_area(self)
 
 
+@warning_ignore("unused_parameter")
 func _on_body_exited(body: Node2D) -> void:
 	InteractionManager.unregister_area(self)
