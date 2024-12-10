039c03c543457829a44aae9eb33db5aa59b51150
Mctaowing <mikkelchristensen2001@gmail.com>
2024-12-10 21:42:25 +0100

    Update dungeon_door.gd

--- Scenes/town/dungeon_door.gd
+++ Scenes/town/dungeon_door.gd
@@ -13,9 +13,9 @@ func _process(delta: float) -> void:
 
 func _on_interact():
 	global.transition_scene = true
-	change_scene()
+	_change_scene()
 
-func change_scene():
+func _change_scene():
 	if global.transition_scene == true:
 		if global.current_scene == "game":
 			get_tree().change_scene_to_file("res://Scenes/DungeonGenerator/dungeon_generator.tscn")
