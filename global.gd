7903a44e90cdee48d4bde3b720fcd13d31768759
Mctaowing <mikkelchristensen2001@gmail.com>
2026-02-02 12:20:22 +0100

    Update global.gd

--- global.gd
+++ global.gd
@@ -1,7 +1,10 @@
 extends Node
 
 var gold = 0
-var exp = 0
+var durries = 0
+var player_exp = 0
+var player_level = 1
+var exp_thresholds = [0, 1000, 2000, 4000, 8000, 16000]
 
 var transition_scene = false
 var current_scene = "game"
